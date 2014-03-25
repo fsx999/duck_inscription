8fd9d4b59a746e9185cc558ef51a4326eeddce82
paul <paul.guichon@iedparis8.net>
2014-03-25 15:29:53 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='duck_inscription', version='0.1', packages=['duck_inscription'], url='', license='',
+      author='paul guichon', author_email='paul.guichon@iedparis8.net', description='',
+      install_requires=['django',
+                        'south',
+                        'cx_Oracle',
+                        'django_apogee',
+                        'django-registration'])
