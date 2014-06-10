ba9063070854360950e80798612c6fe0ae0710bf
paul <paul.guichon@iedparis8.net>
2014-06-10 16:37:12 +0200

    modif liste attente

--- test_duck_inscription/import_settings.py
+++ test_duck_inscription/import_settings.py
@@ -106,7 +106,17 @@ TEMPLATE_DIRS = (
 )
 
 ACCOUNT_ACTIVATION_DAYS = 1
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "sekizai.context_processors.sekizai",
+)
 MEDIA_URL = '/static_tel/'
 HAYSTACK_CONNECTIONS = {
     'default': {
