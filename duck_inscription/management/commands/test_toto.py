9fcb1b141b1318f34145f5e98a106e97a6b7c2db
paul <paul.guichon@iedparis8.net>
2014-06-17 14:40:59 +0200

    Revert "modif liste attente"
    This reverts commit 5997e7cea5d3cf6901312dd833842e4495193c41.

--- duck_inscription/management/commands/test_toto.py
+++ duck_inscription/management/commands/test_toto.py
@@ -4,8 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import IntegrityError
 from django_xworkflows.xworkflow_log.models import TransitionLog
 from django_apogee.models import AnneeUni, VersionEtape as VersionEtapeApogee, EtpGererCge, Etape
-from duck_inscription.models import SettingAnneeUni, SettingsEtape, Wish, WishTransitionLog, WishParcourTransitionLog, \
-    IndividuTransitionLog
+from duck_inscription.models import SettingAnneeUni, SettingsEtape, Wish, WishTransitionLog, WishParcourTransitionLog
 
 __author__ = 'paul'
 from django.core.management.base import BaseCommand
@@ -15,20 +14,20 @@ APOGEE_CONNECTION = getattr(settings, 'APOGEE_CONNECTION', 'oracle')
 class Command(BaseCommand):
     def handle(self, *args, **options):
         # on récupére les personnes du jour (soit la date de création, de modif plus grand que la veille
-        t = ContentType.objects.get(model='individu', app_label='duck_inscription')
+        t = ContentType.objects.get(model='wish')
 
-        for trans in TransitionLog.objects.filter(content_type=t):
+        for trans in TransitionLog.objects.filter(content_type=t).exclude(transition='equivalence_receptionner'):
             try:
-                IndividuTransitionLog.objects.create(
+                WishParcourTransitionLog.objects.create(
                     transition=trans.transition,
                     from_state=trans.from_state,
                     to_state=trans.to_state,
                     timestamp=trans.timestamp,
-                    individu_id=trans.content_id
+                    wish_id=trans.content_id
                 )
             except IntegrityError:
                 pass
-        print IndividuTransitionLog.objects.all().count()
+        print WishParcourTransitionLog.objects.all().count()
         # print type(a.modified_object)
         # wishes = Wish.objects.filter(pk__in=pks, etape__cod_etp__in=['L1NPSY', 'L2NPSY', 'L3NPSY'])
         # print wishes
