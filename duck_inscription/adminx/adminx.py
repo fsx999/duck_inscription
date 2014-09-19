79d22a8c0512241ff8783aa9c21660280e0a71a1
paul <paul.guichon@iedparis8.net>
2014-09-19 17:11:08 +0200

    ajout dossier inscription

--- duck_inscription/adminx/adminx.py
+++ duck_inscription/adminx/adminx.py
@@ -28,8 +28,8 @@ from openpyxl import Workbook
 
 class IncriptionDashBoard(views.website.IndexView):
     widgets = [[{"type": "qbutton", "title": "Gestion dossier",
-                 "btns": [{'title': 'Reception', 'url': 'dossier_receptionner'},
-                          {'title': 'Gestion Equivalence', 'url': 'dossier_equivalence'},
+                 "btns": [{'title': 'Reception', 'url': '/dossier_receptionner'},
+                          {'title': 'Gestion Equivalence', 'url': '/dossier_equivalence'},
                           {'title': 'Gestion Dossier inscription', 'url': '/traitement_inscription'}, ]},
                 {"type": "qbutton", "title": "Consultation des dossiers",
                 "btns": [{'title': 'Consultation dossier inscription', 'model': Individu}, ]}
@@ -44,8 +44,8 @@ xadmin.site.register_view(r'^inscription/$', IncriptionDashBoard, 'inscription')
 
 class StatistiqueDashBoard(views.website.IndexView):
     widgets = [[{"type": "qbutton", "title": "Inscription",
-                 "btns": [{'title': 'Statistique Pal (équivalence, candidature)', 'url': 'stats_pal'},
-                          {'title': 'Statistique Piel (préinscription)', 'url': 'stats_piel'}, ]}, ]]
+                 "btns": [{'title': 'Statistique Pal (équivalence, candidature)', 'url': '/stats_pal'},
+                          {'title': 'Statistique Piel (préinscription)', 'url': '/stats_piel'}, ]}, ]]
     site_title = 'Backoffice'
     title = 'Accueil'
     widget_customiz = False
