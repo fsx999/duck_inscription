27f0dd0f9701ea8666d4811c0f2681169d50c0bd
paul guichon <paul.guichon@iedparis8.net>
2014-12-12 14:47:32 +0100

    ajout ip

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 
-setup(name='duck_inscription', version='',
-      packages=['duck_theme_ied', 'duck_inscription', 'duck_inscription.urls', 'duck_inscription.admin',
-                'duck_inscription.forms', 'duck_inscription.views', 'duck_inscription.adminx',
-                'duck_inscription.adminx.html_string', 'duck_inscription.models', 'duck_inscription.managers',
-                'duck_inscription.management', 'duck_inscription.management.commands', 'duck_inscription.migrations',
-                'duck_inscription.templatetags', 'duck_inscription.xadmin_plugins'], url='', license='',
-      author='paul guichon', author_email='paul.guichon@iedparis8.net', description='')
+setup(
+    name='duck_inscription',
+    version='',
+    packages=['', 'duck_theme_ied', 'duck_inscription', 'duck_inscription.urls', 'duck_inscription.admin',
+              'duck_inscription.forms', 'duck_inscription.views', 'duck_inscription.adminx',
+              'duck_inscription.adminx.html_string', 'duck_inscription.models', 'duck_inscription.managers',
+              'duck_inscription.management', 'duck_inscription.management.commands', 'duck_inscription.templatetags',
+              'duck_inscription.xadmin_plugins', 'duck_inscription.south_migrations'],
+    url='',
+    license='',
+    author='paulguichon',
+    author_email='',
+    description=''
+)
