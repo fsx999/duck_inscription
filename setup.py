c206c5d97995d77cf5989ec827f433ebc766d898
paul guichon <paul.guichon@iedparis8.net>
2015-03-31 13:47:41 +0200

    modif pour le ctu

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='duck_inscription',
     version='',
-    packages=['', 'duck_theme_ied', 'duck_inscription', 'duck_inscription.urls', 'duck_inscription.admin',
+    packages=['', 'duck_inscription', 'duck_inscription.urls', 'duck_inscription.admin',
               'duck_inscription.forms', 'duck_inscription.views', 'duck_inscription.adminx',
               'duck_inscription.adminx.html_string', 'duck_inscription.models', 'duck_inscription.managers',
               'duck_inscription.management', 'duck_inscription.management.commands', 'duck_inscription.templatetags',
