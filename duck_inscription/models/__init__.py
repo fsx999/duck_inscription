214a62cd9e382045e0f6f962aba91f6b362de91f
paul guichon <paul.guichon@iedparis8.net>
2015-04-02 12:31:10 +0200

    modif pour helpdesk

--- duck_inscription/models/__init__.py
+++ duck_inscription/models/__init__.py
@@ -5,4 +5,5 @@ from .individu_models import *
 from .wish_models import *
 from .workflows_models import *
 from .user_models import *
-__all__ = ['apogee_models', 'individu_models', 'wish_models', 'workflows_models', 'user_models']
+from .auth_models import *
+__all__ = ['apogee_models', 'individu_models', 'wish_models', 'workflows_models', 'user_models', 'apogee_models', 'auth_models']
---
+++ duck_inscription/models/auth_models.py
@@ -0,0 +1,134 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from django.contrib import auth
+from django.contrib.auth.models import AbstractUser, UserManager, Group, Permission, _user_get_all_permissions
+from django.core import validators
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
+def _user_has_perm(user, perm, obj):
+    for backend in auth.get_backends():
+        if hasattr(backend, "has_perm"):
+            if backend.has_perm(user, perm, obj):
+                return True
+    return False
+
+
+def _user_has_module_perms(user, app_label):
+    for backend in auth.get_backends():
+        if hasattr(backend, "has_module_perms"):
+            if backend.has_module_perms(user, app_label):
+                return True
+    return False
+
+
+class InscriptionUser(models.Model):
+    username = models.CharField(_('username'), max_length=30, unique=True,
+                                help_text=_('Required. 30 characters or fewer. Letters, digits and '
+                                            '@/./+/-/_ only.'),
+                                validators=[
+                                    validators.RegexValidator(r'^[\w.@+-]+$', _('Enter a valid username.'), 'invalid')
+                                ])
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin '
+                                               'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+                                    help_text=_('Designates whether this user should be treated as '
+                                                'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    is_superuser = models.BooleanField(_('superuser status'), default=False,
+                                       help_text=_('Designates that this user has all permissions without '
+                                                   'explicitly assigning them.'))
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'),
+                                    blank=True, help_text=_('The groups this user belongs to. A user will '
+                                                            'get all permissions granted to each of '
+                                                            'his/her group.'),
+                                    related_name="user_inscription_set", related_query_name="user")
+    user_permissions = models.ManyToManyField(Permission,
+                                              verbose_name=_('user permissions'), blank=True,
+                                              help_text=_('Specific permissions for this user.'),
+                                              related_name="user_inscription_set", related_query_name="user")
+
+    def get_group_permissions(self, obj=None):
+        """
+        Returns a list of permission strings that this user has through their
+        groups. This method queries all available auth backends. If an object
+        is passed in, only permissions matching this object are returned.
+        """
+        permissions = set()
+        for backend in auth.get_backends():
+            if hasattr(backend, "get_group_permissions"):
+                permissions.update(backend.get_group_permissions(self, obj))
+        return permissions
+
+    def get_all_permissions(self, obj=None):
+        return _user_get_all_permissions(self, obj)
+
+    def has_perm(self, perm, obj=None):
+        """
+        Returns True if the user has the specified permission. This method
+        queries all available auth backends, but returns immediately if any
+        backend returns True. Thus, a user who has permission from a single
+        auth backend is assumed to have permission in general. If an object is
+        provided, permissions for this specific object are checked.
+        """
+
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        # Otherwise we need to check the backends.
+        return _user_has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        """
+        Returns True if the user has each of the specified permissions. If
+        object is passed, it checks if the user has all required perms for this
+        object.
+        """
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+
+    def has_module_perms(self, app_label):
+        """
+        Returns True if the user has any permissions in the given app label.
+        Uses pretty much the same logic as has_perm, above.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and self.is_superuser:
+            return True
+
+        return _user_has_module_perms(self, app_label)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    class Meta:
+        db_table = 'inscription_user'
