0978e2307280c4917bddefee67c4b8a825bb4d8e
paul guichon <paul.guichon@iedparis8.net>
2015-04-17 16:26:03 +0200

    ajout envoi mail auditeur

--- duck_inscription/apps.py
+++ duck_inscription/apps.py
@@ -3,8 +3,3 @@ from __future__ import unicode_literals
 __author__ = 'paulguichon'
 from django.apps import AppConfig
 
-
-class DuckInscriptionConfig(AppConfig):
-    verbose_name = 'inscription'
-    models_module = 'models'
-
