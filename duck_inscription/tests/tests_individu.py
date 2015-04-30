f9b18172074d06fcb7adedec2ee992c16393f226
paul guichon <paul.guichon@iedparis8.net>
2015-04-30 14:29:38 +0200

    bug code_etudiant

--- duck_inscription/tests/tests_individu.py
+++ duck_inscription/tests/tests_individu.py
@@ -1,4 +1,9 @@
 # coding=utf-8
+from django_apogee.factories.individu_factories import IndividuFactory
+from django_apogee.models import Individu
+from datetime import date
+from duck_inscription.forms import CodeEtudiantForm
+
 __author__ = 'paulguichon'
 from django.test import TestCase
 
@@ -6,7 +11,19 @@ from django.test import TestCase
 class IndividuTestCase(TestCase):
 
     def setUp(self):
-        print "coucou"
+        IndividuFactory.create_individu_with_two_adresses()
 
-    def test_individu(self):
-        print "re coucou"
+    def test_individu_code_form(self):
+        individu = Individu.objects.all().first()
+
+        date_naissance_false = date.today()
+        cod_etu = individu.cod_etu
+        form = CodeEtudiantForm(data={'date_naissance': individu.date_nai_ind,
+                                      'code_etu': individu.cod_etu})
+        self.assertTrue(form.is_valid())
+        form = CodeEtudiantForm(data={'date_naissance': date_naissance_false,
+                                      'code_etu': individu.cod_etu})
+        self.assertFalse(form.is_valid())
+        form = CodeEtudiantForm(data={'date_naissance': date_naissance_false,
+                                      'code_etu': '0'})
+        self.assertFalse(form.is_valid())
